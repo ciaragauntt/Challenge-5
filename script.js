6e34c9eb5de109df1a6e514ea2a6b9215b141080
Ciara Gauntt <ci.gauntt@gmail.com>
2022-05-20 09:43:22 -0600

    fixed javascript function final commit

--- script.js
+++ script.js
@@ -11,19 +11,19 @@ function timeTracker(){
     var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
     if (blockTime < currentHour) {
-        $(this).addClass("past");
         $(this).removeClass("future");
         $(this).removeClass("present");
+        $(this).addClass("past");
     }
     else if (blockTime === currentHour) {
         $(this).removeClass("past");
-        $(this).addClass("future");
-        $(this).removeClass("present");
+        $(this).removeClass("future");
+        $(this).addClass("present");
     }
     else {
         $(this).removeClass("past");
-        $(this).removeClass("future");
-        $(this).addClass("present");
+        $(this).removeClass("present");
+        $(this).addClass("future");
     }
 })
 }
@@ -42,9 +42,9 @@ $(document).ready(function(){
     $("#hour10 .description").val(localStorage.getItem("hour10"));
     $("#hour11 .description").val(localStorage.getItem("hour11"));
     $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour1 .description").val(localStorage.getItem("hour1"));
-    $("#hour2 .description").val(localStorage.getItem("hour2"));
-    $("#hour3 .description").val(localStorage.getItem("hour3"));
-    $("#hour4 .description").val(localStorage.getItem("hour4"));
-    $("#hour5 .description").val(localStorage.getItem("hour5"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
 })
